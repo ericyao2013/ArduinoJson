e448ebfc7cb226cb76112dafe887f939317b9cfd
Benoit Blanchon <github@benoitblanchon.fr>
2017-07-03 21:42:52 +0200

    Almost OK

--- src/ArduinoJson/Polyfills/constPow10.hpp
+++ src/ArduinoJson/Polyfills/constPow10.hpp
@@ -12,7 +12,31 @@ namespace Polyfills {
 
 template <int N>
 inline double constPow10() {
-  return constPow10<N / 2>() * constPow10<N / 2>() * constPow10<N / 2>();
+  return constPow10<N / 2>() * constPow10<N / 2>() * constPow10<N % 2>();
+}
+template <>
+inline double constPow10<-32>() {
+  return 1e-32;
+}
+template <>
+inline double constPow10<-16>() {
+  return 1e-16;
+}
+template <>
+inline double constPow10<-8>() {
+  return 1e-8;
+}
+template <>
+inline double constPow10<-4>() {
+  return 1e-4;
+}
+template <>
+inline double constPow10<-3>() {
+  return 1e-3;
+}
+template <>
+inline double constPow10<-2>() {
+  return 1e-2;
 }
 template <>
 inline double constPow10<-1>() {
@@ -25,6 +49,26 @@ inline double constPow10<0>() {
 template <>
 inline double constPow10<1>() {
   return 1e1;
+}
+template <>
+inline double constPow10<2>() {
+  return 1e2;
+}
+template <>
+inline double constPow10<4>() {
+  return 1e4;
+}
+template <>
+inline double constPow10<8>() {
+  return 1e8;
+}
+template <>
+inline double constPow10<16>() {
+  return 1e16;
+}
+template <>
+inline double constPow10<32>() {
+  return 1e32;
 }
 }
 }
